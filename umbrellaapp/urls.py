86e609c388cf7698d0f976429f979552e1d64f97
yoo-hwan <youh5325@naver.com>
2024-06-09 15:03:32 +0900

    feat: sse url 추가

--- umbrellaapp/urls.py
+++ umbrellaapp/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('sse/', views.humid_event_sse, name='sse'),
     path('save_temperature_humidity_data/', views.save_temperature_humidity_data, name='save_temperature_humidity_data'),
     path('get_humidity/', views.get_humidity, name='get_humidity'),
     path('save_ultrasonic_data/', views.save_ultrasonic_data, name='save_ultrasonic_data'),
