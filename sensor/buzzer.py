9a9dee1ed4c99e37988688ea8ac4025e1eb38d18
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2024-06-13 00:40:09 +0900

    feat: 부저 울림 함수 추가

--- sensor/buzzer.py
+++ sensor/buzzer.py
@@ -2,22 +2,25 @@ import RPi.GPIO as GPIO
 import time
 
 buzzer = 20
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(buzzer, GPIO.OUT)
-GPIO.setwarnings(False)
 
-pwm = GPIO.PWM(buzzer, 1.0)
-pwm.start(50.0)
+# 부저 울림
+def turn_on_buzzer(state):
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(buzzer, GPIO.OUT)
+    GPIO.setwarnings(False)
 
-for cnt in range(0,3):
-    pwm.ChangeFrequency(262)
-    time.sleep(1.0)
-    pwm.ChangeFrequency(294)
-    time.sleep(1.0)
-    pwm.ChangeFrequency(330)
-    time.sleep(1.0)
+    pwm = GPIO.PWM(buzzer, 1.0)
+    pwm.start(50.0)
 
-pwm.ChangeDutyCycle(0.0)
+    for cnt in range(0,3):
+        pwm.ChangeFrequency(262)
+        time.sleep(1.0)
+        pwm.ChangeFrequency(294)
+        time.sleep(1.0)
+        pwm.ChangeFrequency(330)
+        time.sleep(1.0)
 
-pwm.stop()
-GPIO.cleanup()
+    pwm.ChangeDutyCycle(0.0)
+
+    pwm.stop()
+    GPIO.cleanup()
