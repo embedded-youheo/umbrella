b4c16c5660aca546f83bbfec68cf83095cd2ff9f
yoo-hwan <youh5325@naver.com>
2024-06-17 13:50:18 +0900

    wsgi 수정

--- umbrella/wsgi.py
+++ umbrella/wsgi.py
@@ -8,9 +8,34 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
+import threading
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'umbrella.settings')
 
 application = get_wsgi_application()
+
+def start_listen_to_notifications():
+    from django.core.management import call_command
+    call_command('listen_to_notifications')
+
+def start_humidity_sensor():
+    from sensor.dht22 import observe_humidity
+    observe_humidity()
+
+def start_ultrasonic_sensor():
+    from sensor.ultrasonic import observe_entry_exit
+    observe_entry_exit()
+
+notification_thread = threading.Thread(target=start_listen_to_notifications)
+notification_thread.daemon = True
+notification_thread.start()
+
+sensor_thread = threading.Thread(target=start_humidity_sensor)
+sensor_thread.daemon = True
+sensor_thread.start()
+
+ultrasonic_thread = threading.Thread(target=start_ultrasonic_sensor)
+ultrasonic_thread.daemon = True
+ultrasonic_thread.start()
